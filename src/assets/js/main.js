4ab8897723444c91f26b3f0d111c19d9a26f5266
maia rojas <maia.rt.46@gmail.com>
2017-09-24 01:15:14 -0500

    Base de datos iniciales

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,37 +1,36 @@
 "use strict";
 
-
 const root = $(".root");
 const render = (root) => {
     root.empty();
     const wrapper = $('<div class="wrapper"></div>');
 
-    switch (state.pagina) {
-        case null:
-            wrapper.append(Home(updated));
-            break;
-        case 1:
-            wrapper.append(Recicla(updated));
-            break;
-        case 2:
-            wrapper.append(MapaRecicla(updated));
-            break;
-        case 3:
-            wrapper.append(RutaRecicla(updated));
-            break;
-        case 4:
-            wrapper.append(FormAcopio(updated));
-            break;
-        case 5:
-            wrapper.append(SuccesAcopio(updated));
-            break;
-        case 6:
-            wrapper.append(TipsR(updated));
-            break;
-        case 7:
-            wrapper.append(TipDetail(updated));
-            break;
-    };
+    // switch (state.pagina) {
+    //     case null:
+    //         wrapper.append(Home(updated));
+    //         break;
+    //     case 1:
+    //         wrapper.append(Recicla(updated));
+    //         break;
+    //     case 2:
+    //         wrapper.append(MapaRecicla(updated));
+    //         break;
+    //     case 3:
+    //         wrapper.append(RutaRecicla(updated));
+    //         break;
+    //     case 4:
+    //         wrapper.append(FormAcopio(updated));
+    //         break;
+    //     case 5:
+    //         wrapper.append(SuccesAcopio(updated));
+    //         break;
+    //     case 6:
+    //         wrapper.append(TipsR(updated));
+    //         break;
+    //     case 7:
+    //         wrapper.append(TipDetail(updated));
+    //         break;
+    // };
 
     root.append(wrapper);
 
@@ -46,7 +45,26 @@ const state = {
 
 
 $(_ => {
-   
+
+  var config = {
+    apiKey: "AIzaSyBdR77WTmQPJ4ByaPCsNRxJiKSxpPxYmfU",
+    authDomain: "samugo-ffbbd.firebaseapp.com",
+    databaseURL: "https://samugo-ffbbd.firebaseio.com",
+    projectId: "samugo-ffbbd",
+    storageBucket: "samugo-ffbbd.appspot.com",
+    messagingSenderId: "955932477926"
+  };
+
+   firebase.initializeApp(config);
+
+   var database = firebase.database();
+
+    database.ref().on("value", function(snap){
+    console.log("holi");
+    state.samugo = snap.val();
+    console.log(state.samugo);
+
     const root = $(".root");
     render(root);
+  });
 });
